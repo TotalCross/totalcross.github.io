6c82739530d09e70ff15fd04786584d9b94d7ff7
flsobral <flsobral@users.noreply.github.com>
2022-08-22 20:47:06 +0000

    deployed via Gatsby Publish Action 🎩 for 999f35b34166e1a7eaf51ff72dc39eabc0fbb4ca

---
+++ component---src-pages-404-js-331f49a6c9ac4cea602e.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[6],{w2l6:function(n,t,e){"use strict";e.r(t);var c=e("q1tI"),u=e("Wbzz");t.default=function(){return Object(c.useEffect)((function(){Object(u.navigate)("/")}),[]),null}}}]);
+//# sourceMappingURL=component---src-pages-404-js-331f49a6c9ac4cea602e.js.map
